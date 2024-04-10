7b040672197d176305d4ab340add4b83bfabd9a3
davidmrtn07 <davidmrtn07@gmail.com>
2024-04-10 12:14:44 -0500

    first exercise completed

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //dot way 
+            Car myCar = new Car();
+            myCar.Make = "Ford";
+            myCar.Model = "Focus";
+            myCar.Year = 2013;
+
+            //object initializer way
+            var Mazda = new Car()
+            {
+                Make = "Mazda",
+                Model = "CX-5",
+                Year = 2016
+            };
+
+            //passing in using constructor
+            var chevy = new Car("Chevy", "Impala", 2001);
+            var carList = new List<Car>() { myCar, Mazda, chevy };
+
+            foreach (var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model}{vehicle.Year}");
+            }
         }
     }
 }
